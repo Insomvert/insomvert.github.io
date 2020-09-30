21e97211cf8570bdf3ae9041fb18d3416349be4f
insomvert <frydwin@gmail.com>
2020-09-30 20:11:37 +0800

    initial commit

---
+++ precache-manifest.5dbd743a58f0224814c52dfe10fa81d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36879f5638195773ce9e263813a78f22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f392753be814490da826",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1e05aca8787aee1fe300",
+    "url": "/static/css/main.07dce959.chunk.css"
+  },
+  {
+    "revision": "f392753be814490da826",
+    "url": "/static/js/2.43c9bd1a.chunk.js"
+  },
+  {
+    "revision": "e95b3ebaec68caad50c330e63af5be13",
+    "url": "/static/js/2.43c9bd1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e05aca8787aee1fe300",
+    "url": "/static/js/main.f7dc9304.chunk.js"
+  },
+  {
+    "revision": "825dad03ba05ee74ed4a",
+    "url": "/static/js/runtime-main.87d86c70.js"
+  },
+  {
+    "revision": "99b8cb1816a2bb0b46df14f0fe5ae3bd",
+    "url": "/static/media/logo.99b8cb18.svg"
+  }
+]);
